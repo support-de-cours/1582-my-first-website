dd7001650d1d616b602f8f5ab654866939d2dac1
Arnaud BODEL <contact@osw3.net>
2022-09-27 16:08:41 +0200

    ajout de ejs

--- index.js
+++ index.js
@@ -17,21 +17,21 @@ const port = 3000;
 // Definition du moteur de rendu,
 // On precise à Express que le moteur de rendu des vues se fera avec le module "ejs"
 // Le module "ejs", transmet à Express ses paramètres avec la propriete "__express"
-app.engine('.ejs', ejs.__express);
+app.engine('.html', ejs.__express);
 
 // Définition du répertoire de stockage des fichiers de vues
 // path.join() va resoudre automatiquement le chemin absolut du repertoire "views"
 app.set('views', path.join(__dirname, "views"));
 
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 
 
 
 // Routing
 // --
 
-app.use( "/", require('./controllers/homepage'));
+app.use("/", require('./controllers/homepage'));
 
 
 
